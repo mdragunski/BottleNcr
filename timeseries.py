d95fadf296a749544538e993c904360a7cc589c0
Nico Steffens <n.steffens@uni-muenster.de>
2017-07-21 16:31:10 +0200

    Added speed time series
    Basic time series for speed. UI optimization still to be done..

--- timeseries.py
+++ timeseries.py
@@ -1,18 +1,31 @@
+import arcpy
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-import datetime
+import datetime as dt
 import time
 
-value = range(1, 20)
-ti = []
+speed_data = []
+time_data = []
 
-for i in range(1,20):
-    ti.append(datetime.datetime.now())
-    time.sleep(1)
+def plot(input_shp):
+    searchCursor = arcpy.SearchCursor(input_shp)
     
-fig, ax = plt.subplots()
-ax.plot_date(x=ti, y=value, fmt="r-")
-ax.set_xlim(ti[0], ti[len(ti)-1])
-ax.grid(True)
-fig.show()
+    for row in searchCursor:
+        speed_data.append(row.getValue("bttlNcr"))
+        fmt = "%Y-%m-%d_%H-%M-%S"
+        date = dt.datetime.strptime(row.getValue("date"), fmt)
+        time_data.append(date)
+    
+    displayPlot()
+    del searchCursor
+    
+def displayPlot():
+    fig, ax1 = plt.subplots(nrows = 1, sharex = True)
+    #secondLocator = mdates.SecondLocator()
+    #ax1.xaxis.set_minor_locator(secondLocator)
+    ax1.plot(time_data, speed_data, 'r-')
+
+    plt.grid(True)
+    plt.show()
+
