39a379e117d95aa61e3521249662959fd46b7853
Nico Steffens <n.steffens@uni-muenster.de>
2017-07-21 16:29:37 +0200

    Adjusted url
    Import from file to not use ArcGIS...

--- BottleNcr.py
+++ BottleNcr.py
@@ -1,18 +1,19 @@
-import FinalScript
-import validator
-
 import arcpy
 
+import FinalScript
+import validator
+import timeseries
 
-points = arcpy.GetParameterAsText(0)
+#points = arcpy.GetParameterAsText(0)
+points = "C:\\Users\\nico\\Desktop\\EoTData\\smartphone_data\\tp1_17-17-11_nach-abpfiff.shp"
 
 validated_shapefile = validator.validateShapefile(points)
-pointsNew = FinalScript.calculateSpeed(validated_shapefile)
-
+points_with_speed = FinalScript.calculateSpeed(validated_shapefile)
+timeseries.plot(points_with_speed)
 
 # adding new shapefile to ArcMap 
 mxd = arcpy.mapping.MapDocument("CURRENT")
 df = arcpy.mapping.ListDataFrames(mxd, "*")[0]
-newLayer = arcpy.mapping.Layer(pointsNew)
+newLayer = arcpy.mapping.Layer(points_with_speed)
 arcpy.ApplySymbologyFromLayer_management(newLayer, "points_new_style.lyr")
 arcpy.mapping.AddLayer(df, newLayer, "TOP")
