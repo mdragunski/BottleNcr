4537f180554c7bd6ca145bb9699331931d16de9d
Nico Steffens <n.steffens@uni-muenster.de>
2017-07-22 13:19:40 +0200

    Added Histogram, Accuracy Plot
    - added histogram of speed values
    - added accuracy time series
    - added labels and other design elements --> styling

--- timeseries.py
+++ timeseries.py
@@ -1,31 +1,105 @@
 import arcpy
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import matplotlib.patches as mpatches
 import datetime as dt
+import numpy as np
 import time
 
 speed_data = []
+accuracy_data = []
 time_data = []
 
-def plot(input_shp):
+
+def init(input_shp):
+
     searchCursor = arcpy.SearchCursor(input_shp)
-    
+
     for row in searchCursor:
         speed_data.append(row.getValue("bttlNcr"))
+        accuracy_data.append(row.getValue("accuracy"))
         fmt = "%Y-%m-%d_%H-%M-%S"
         date = dt.datetime.strptime(row.getValue("date"), fmt)
         time_data.append(date)
-    
-    displayPlot()
     del searchCursor
-    
-def displayPlot():
+
+    displaySpeedPlot()
+    displayAccuracyPlot()
+    displaySpeedHist()
+
+
+def displaySpeedPlot():
     fig, ax1 = plt.subplots(nrows = 1, sharex = True)
-    #secondLocator = mdates.SecondLocator()
-    #ax1.xaxis.set_minor_locator(secondLocator)
-    ax1.plot(time_data, speed_data, 'r-')
 
+    mean = np.mean(speed_data)
+    mean_data = [mean] * len(time_data)
+
+    # Styling
+    xfmt = mdates.DateFormatter('%H:%M:%S')
+    ax1.xaxis.set_major_formatter(xfmt)
     plt.grid(True)
-    plt.show()
+    ax1.plot(time_data, speed_data, 'r-')
+    ax1.plot(time_data, mean_data, 'b-')
 
+    # Titles
+    fig.suptitle("Speed over Time")
+    ax1.set_xlabel("t")
+    ax1.set_ylabel("km / h")
+
+    # Legend
+
+    speedPatch = mpatches.Patch(color='red', label='Speed in km/h')
+
+    labelString = "Average Speed: %s km/h" % round(mean,3)
+    averagePatch = mpatches.Patch(color='blue', label=labelString)
+
+    plt.legend(handles=[speedPatch,averagePatch])
+
+    #save as png
+    imageOut = arcpy.env.scratchFolder + os.sep + "speed.png"
+    fig.savefig(imageOut, dpi=fig.dpi)
+
+
+def displayAccuracyPlot():
+    fig, ax1 = plt.subplots(nrows = 1, sharex = True)
+
+    mean = np.mean(accuracy_data)
+    mean_data = [mean] * len(time_data)
+
+    # Styling
+    xfmt = mdates.DateFormatter('%H:%M:%S')
+    ax1.xaxis.set_major_formatter(xfmt)
+    plt.grid(True)
+    ax1.plot(time_data, accuracy_data, 'r-')
+    ax1.plot(time_data, mean_data, 'b-')
+
+    # Titles
+    fig.suptitle("Speed over Time")
+    ax1.set_xlabel("t")
+    ax1.set_ylabel("km / h")
+
+    # Legend
+    accuracyPatch = mpatches.Patch(color='red', label="Accuracy in meters")
+    averagePatch = mpatches.Patch(color='blue', label="Average Accuracy")
+
+    plt.legend(handles=[accuracyPatch,averagePatch])
+
+    #save as png
+    imageOut = arcpy.env.scratchFolder + os.sep + "accuracy.png"
+    fig.savefig(imageOut, dpi=fig.dpi)
+
+
+def displaySpeedHist():
+
+    fig = plt.figure()
+    plt.hist(speed_data)
+
+    plt.xlabel('Speed Values')
+    plt.ylabel('Frequency')
+
+    plt.title('Frequency of Speed Values')
+
+    imageOut = arcpy.env.scratchFolder + os.sep + "hist.png"
+    fig.savefig(imageOut, dpi=fig.dpi)
